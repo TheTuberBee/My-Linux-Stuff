7bed64a0dbee829db4b5ea67fb07e1e65c7e01bf
Dénes Balogh <denes999x@gmail.com>
2022-05-23 18:45:05 +0200

    .

--- setup.sh
+++ setup.sh
@@ -201,17 +201,15 @@ sudo swapon swapfile
 echo "/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab
 echo "vm.swappiness=20" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
 
-cd /home/frt
-mkdir -p Repositories
-cd Repositories
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cd /home/frt/Desktop
-cd /home/frt/Asztal
+cd /home/frt/Desktop || true
+cd /home/frt/Asztal || true
 wget -O tlauncher.zip https://tlauncher.org/jar
 unzip tlauncher.zip
 cp -R tlauncher/. .
 rm -R tlauncher.zip
 rm -R tlauncher
 
-sudo usermod --shell /bin/zsh frt
+cd /home/frt
+mkdir -p Repositories
+cd Repositories
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- setup_modified.sh
+++ setup_modified.sh
@@ -2,17 +2,10 @@
 # arch linux after install setup script
 set -e
 
-cd /home/frt
-mkdir -p Repositories
-cd Repositories
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cd /home/frt/Desktop
-cd /home/frt/Asztal
+cd /home/frt/Desktop || true
+cd /home/frt/Asztal || true
 wget -O tlauncher.zip https://tlauncher.org/jar
 unzip tlauncher.zip
 cp -R tlauncher/. .
 rm -R tlauncher.zip
 rm -R tlauncher
-
-sudo usermod --shell /bin/zsh frt
