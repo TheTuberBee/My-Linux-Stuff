db560054f4555576551ac3f3daad720cff253a96
Dénes Balogh <denes999x@gmail.com>
2022-05-23 18:46:29 +0200

    .

--- setup.sh
+++ setup.sh
@@ -205,9 +205,9 @@ cd /home/frt/Desktop || true
 cd /home/frt/Asztal || true
 wget -O tlauncher.zip https://tlauncher.org/jar
 unzip tlauncher.zip
-cp -R tlauncher/. .
-rm -R tlauncher.zip
-rm -R tlauncher
+cp -R tlauncher/. . || true
+rm -R tlauncher.zip || true
+rm -R tlauncher || true
 
 cd /home/frt
 mkdir -p Repositories
--- setup_modified.sh
+++ setup_modified.sh
@@ -5,7 +5,7 @@ set -e
 cd /home/frt/Desktop || true
 cd /home/frt/Asztal || true
 wget -O tlauncher.zip https://tlauncher.org/jar
-unzip tlauncher.zip
-cp -R tlauncher/. .
-rm -R tlauncher.zip
-rm -R tlauncher
+unzip -d tlauncher tlauncher.zip
+cp -R tlauncher/. . || true
+rm -R tlauncher.zip || true
+rm -R tlauncher || true
